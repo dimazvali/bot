60adcd3a58fe5bf4c26b8165954e491aca4ac842
Dmitry Shestakov <dimazvali@gmail.com>
2024-05-13 12:18:51 +0300

    homeless route

--- routes/homelessBot.js
+++ routes/homelessBot.js
@@ -1,5 +1,5 @@
-let ngrok = process.env.ngrok2 
-// let ngrok = process.env.ngrok 
+// let ngrok = process.env.ngrok2 
+let ngrok = process.env.ngrok 
 
 const host = `homeless`;
 const token = process.env.homelessToken;
