f50c75cd6b8a72d6d36654aa50a55cf6e4e82017
Dmitry Shestakov <dimazvali@gmail.com>
2024-12-21 01:43:06 +0300

    gp

---
+++ public/gp/js.js
@@ -0,0 +1,153 @@
+let sizes = {
+    x: 2399,
+    y: 1627
+}
+
+let scrollBlockY, 
+    scrollBlockX,
+    removeLine,
+    scrolling  = false;
+
+
+window.onload=()=>{
+    
+    sizes.y = viewBox.getBoundingClientRect().height/3
+    sizes.x = viewBox.getBoundingClientRect().width/3
+
+    let cpy = window.pageYOffset;
+    let cpx = window.pageXOffset;
+
+    window.scrollTo({
+        left:   sizes.x,
+        top:    sizes.y
+    })
+
+    setTimeout(()=>{
+        window.onscroll=(e)=>{
+    
+            scrolling = true;
+            
+        
+        
+            let left =  window.pageXOffset- sizes.x*2;
+            let top =   window.pageYOffset - sizes.y*2;
+            
+            if(top < -1*(sizes.y/1) && !scrollBlockY){
+                    
+                scrollBlockY = true;
+        
+                let line = document.querySelector(`.line`)
+                let newline = line.cloneNode(true);
+                line.parentElement.prepend(newline);
+        
+                setTimeout(()=>{
+                    scrollBlockY = false;
+                },300)
+                
+            }
+        
+            if(top > 0 && !scrollBlockY){
+                    
+                scrollBlockY = true;
+        
+                let line = document.querySelector(`.line`)
+                let newline = line.cloneNode(true);
+                line.parentElement.append(newline);
+        
+                setTimeout(()=>{
+                    scrollBlockY = false;
+                },300)
+                
+            }
+            
+            if(left < -1*(sizes.x/2) && !scrollBlockX){
+                
+                scrollBlockX = true;
+
+                let line = document.querySelector(`.tile`)
+
+                let newline = [line.cloneNode(true),line.cloneNode(true),line.cloneNode(true)];
+                
+                document.querySelectorAll(`.fl`).forEach((c,i)=>c.prepend(newline[i]));
+        
+                setTimeout(()=>{
+                    scrollBlockX = false;
+                },300)
+            }
+
+            if(left > 0 && !scrollBlockX){
+                scrollBlockX = true;
+
+                let line = document.querySelector(`.tile`)
+
+                let newline = [line.cloneNode(true),line.cloneNode(true),line.cloneNode(true)];
+                
+                document.querySelectorAll(`.fl`).forEach((c,i)=>c.append(newline[i]));
+        
+                setTimeout(()=>{
+                    scrollBlockX = false;
+                },300)
+            }
+        
+            // document.querySelector(`#v`).innerHTML = `${window.pageXOffset}<br>${window.pageYOffset}<br>${sizes.y}`
+        }
+        setInterval(()=>{
+            if(scrolling){
+                if(cpy == window.pageYOffset){
+                    scrollEnd();
+                    scrolling = false;
+                } else {
+                    cpy = window.pageYOffset
+                }
+                if(cpx == window.pageXOffset){
+                    scrollEnd();
+                    scrolling = false;
+                } else {
+                    cpx = window.pageXOffset
+                }
+            }
+        },300)
+    },100)
+    
+}
+
+
+
+
+
+
+function scrollEnd(){
+
+    
+    scrollBlockY = false;
+    scrollBlockX = false;
+    
+    let left = window.pageXOffset- sizes.x*2;
+    let top = window.pageYOffset - sizes.y*2;
+
+    window.scrollTo({
+        left:window.pageXOffset%sizes.x+sizes.x,
+        top:window.pageYOffset%sizes.y+sizes.y
+    })
+        
+    try {
+        document.querySelectorAll(`.fl`).forEach((e,i)=>{
+            e.querySelectorAll(`.tile`).forEach((t,i)=>{
+                if(i>2) t.remove()
+            })
+        })    
+    } catch (error) {
+        // alert(error)
+    }
+
+    try {
+        console.log(document.querySelectorAll(`.fl`).length)
+        document.querySelectorAll(`.fl`).forEach((e,i)=>{
+            console.log(i);
+            if(i>2) e.remove();
+        })    
+    } catch (error) {
+        // alert(error)
+    }
+}
+
