24363b584b6c98d4943e66d73e5bca52d44cdfe6
Dmitry Shestakov <dimazvali@gmail.com>
2024-12-22 22:55:30 +0300

    go

--- public/gp/js.js
+++ public/gp/js.js
@@ -3,252 +3,252 @@ let sizes = {
     y: 1627
 }
 
-let scrollBlockY, 
+let scrollBlockY,
     scrollBlockX,
     removeLine,
-    scrolling, 
+    scrolling,
     startRemoved,
     scrolled,
-    mouseDown  = false;
+    mouseDown = false;
 
 
-function popup(e){
+function popup(e) {
     console.log(mouseDown)
-    if(!scrolled) alert(e)
+    if (!scrolled) alert(e)
 }
 
-function toggleStart(){    
+function toggleStart() {
 
     startRemoved = true;
-    
+
     start.style.filter = 'opacity(0)';
 
-    setTimeout(()=>{
-        
+    setTimeout(() => {
+
         start.remove();
-        
-        document.querySelectorAll(`.hidden`).forEach(i=>{
+
+        document.querySelectorAll(`.hidden`).forEach(i => {
             i.style.display = `initial`
         })
 
-        setTimeout(()=>{
-            document.querySelectorAll(`.hidden`).forEach(i=>{
+        setTimeout(() => {
+            document.querySelectorAll(`.hidden`).forEach(i => {
                 i.classList.toggle(`hidden`)
             })
-        },100)
-        
-    },300)
-    
+        }, 100)
+
+    }, 300)
+
 
 }
 
-window.onbeforeunload = function() {
+window.onbeforeunload = function () {
     window.scrollTo({
-        left:   sizes.x + ((sizes.x-window.innerWidth)/2),
-        top:    sizes.y + ((sizes.y-window.innerHeight)/2)
+        left: sizes.x + ((sizes.x - window.innerWidth) / 2),
+        top: sizes.y + ((sizes.y - window.innerHeight) / 2)
     })
 }
 
-window.onload=()=>{
+window.onload = () => {
 
     hover.style.filter = 'opacity(0)'
-    
-    setTimeout(()=>{
-        hover.remove()
-    },1000)
-    
-    sizes.y = viewBox.getBoundingClientRect().height/3
-    sizes.x = viewBox.getBoundingClientRect().width/3
 
-    let cpy = window.pageYOffset;
-    let cpx = window.pageXOffset;
+    setTimeout(() => {
+        hover.remove();
+        sizes.y = viewBox.getBoundingClientRect().height / 3
+        sizes.x = viewBox.getBoundingClientRect().width / 3
 
-    window.scrollTo({
-        left:   sizes.x + ((sizes.x-window.innerWidth)/2),
-        top:    sizes.y + ((sizes.y-window.innerHeight)/2)
-    })
+        let cpy = window.pageYOffset;
+        let cpx = window.pageXOffset;
 
-    document.onmousedown=(e)=>{
-        e.preventDefault();
-        mouseDown = true;
-        console.log(`вниз`);
-    }
+        window.scrollTo({
+            left: sizes.x + ((sizes.x - window.innerWidth) / 2),
+            top: sizes.y + ((sizes.y - window.innerHeight) / 2)
+        })
 
-    document.onmouseover = (e) => {
-        e.preventDefault()
-    }
-
-    document.onclick=(e)=>{
-        setTimeout(()=>{
-            scrolled = false;
-            // console.log(`сменили мышь на ${false}`)
-        },300)
-        
-        console.log(e)
-        e.cancelBubble = true;
-        e.preventDefault();
-        return false;
-    }
-
-    document.onmouseup=(e)=>{
-        mouseDown = false;
-        console.log(`вверх`)
-    }
-
-    document.onmousemove = (e)=>{
-        e.preventDefault();
-        if(mouseDown) {
-            scrolled = true;
-            window.scrollBy({
-                left:   -e.movementX,
-                top:    -e.movementY
-            })
+        document.onmousedown = (e) => {
+            e.preventDefault();
+            mouseDown = true;
+            console.log(`вниз`);
         }
-    }
 
-    let lastY = 0;
-    let lastX = 0
-
-    setTimeout(()=>{
-        window.onscroll=(e)=>{
-
-
-            if(!startRemoved){
-                toggleStart()
-            }
-    
-            scrolling = true;
-        
-            console.log(lastX,lastY)
-
-            let left =  window.pageXOffset- sizes.x;
-            let top =   window.pageYOffset - sizes.y;
-            
-            console.log(window.pageXOffset,window.pageYOffset);
-
-            // if (window.pageYOffset/sizes.y < 1 && !scrollBlockY && (lastY && (lastY-window.pageYOffset)>0)) {
-
-                
-            //     console.log(`добавляем сверху`)
-            //     scrollBlockY = true;
-
-            //     let line = document.querySelector(`.line`)
-            //     let newline = line.cloneNode(true);
-            //     viewBox.prepend(newline);
-
-            //     setTimeout(() => {
-            //         scrollBlockY = false;
-            //     }, 300)
-
-            // } else if(top > (sizes.y/2) && !scrollBlockY && (lastY && (lastY-window.pageYOffset)<0)){
-
-            //     console.log(`вниз`)
-                    
-            //     scrollBlockY = true;
-        
-            //     let line =          document.querySelector(`.line`)
-            //     let newline =       line.cloneNode(true);
-            //     line.parentElement.append(newline);
-        
-            //     setTimeout(()=>{
-            //         scrollBlockY =  false;
-            //     },300)
-                
-            // }
-            
-            if(lastX){
-                if(lastX>0){
-                    if(left < -1*(sizes.x/2) && !scrollBlockX){
-                
-                        console.log(`влево`)
-        
-                        scrollBlockX = true;
-        
-                        let line = document.querySelector(`.tile`)
-        
-                        let newline = [line.cloneNode(true),line.cloneNode(true),line.cloneNode(true)];
-                        
-                        document.querySelectorAll(`.fl`).forEach((c,i)=>c.prepend(newline[i]));
-                
-                        setTimeout(()=>{
-                            scrollBlockX = false;
-                        },300)
-                    }
-                } else if(lastX<0) {
-                    if (left > 0 && !scrollBlockX){
-
-                        console.log(`вправо`)
-        
-                        scrollBlockX = true;
-        
-                        let line = document.querySelector(`.tile`)
-        
-                        let newline = [line.cloneNode(true),line.cloneNode(true),line.cloneNode(true)];
-                        
-                        document.querySelectorAll(`.fl`).forEach((c,i)=>c.append(newline[i]));
-                
-                        setTimeout(()=>{
-                            scrollBlockX = false;
-                        },300)
-                    }
-                }
-            }
-
-            lastY = window.pageYOffset;
-            lastX = window.pageXOffset;
-        
-            // document.querySelector(`#v`).innerHTML = `${window.pageXOffset}<br>${window.pageYOffset}<br>${sizes.y}`
+        document.onmouseover = (e) => {
+            e.preventDefault()
         }
-        setInterval(()=>{
-            if(scrolling){
-                if(cpy == window.pageYOffset){
-                    scrollEnd();
-                    scrolling = false;
-                } else {
-                    cpy = window.pageYOffset
-                }
-                if(cpx == window.pageXOffset){
-                    scrollEnd();
-                    scrolling = false;
-                } else {
-                    cpx = window.pageXOffset
-                }
+
+        document.onclick = (e) => {
+            setTimeout(() => {
+                scrolled = false;
+                // console.log(`сменили мышь на ${false}`)
+            }, 300)
+
+            console.log(e)
+            e.cancelBubble = true;
+            e.preventDefault();
+            return false;
+        }
+
+        document.onmouseup = (e) => {
+            mouseDown = false;
+            console.log(`вверх`)
+        }
+
+        document.onmousemove = (e) => {
+            e.preventDefault();
+            if (mouseDown) {
+                scrolled = true;
+                window.scrollBy({
+                    left: -e.movementX,
+                    top: -e.movementY
+                })
             }
-        },300)
-    },100)
-    
+        }
+
+        let lastY = 0;
+        let lastX = 0
+
+        setTimeout(() => {
+            window.onscroll = (e) => {
+
+
+                if (!startRemoved) {
+                    toggleStart()
+                }
+
+                scrolling = true;
+
+                console.log(lastX, lastY)
+
+                let left = window.pageXOffset - sizes.x;
+                let top = window.pageYOffset - sizes.y;
+
+                console.log(window.pageXOffset, window.pageYOffset);
+
+                // if (window.pageYOffset/sizes.y < 1 && !scrollBlockY && (lastY && (lastY-window.pageYOffset)>0)) {
+
+
+                //     console.log(`добавляем сверху`)
+                //     scrollBlockY = true;
+
+                //     let line = document.querySelector(`.line`)
+                //     let newline = line.cloneNode(true);
+                //     viewBox.prepend(newline);
+
+                //     setTimeout(() => {
+                //         scrollBlockY = false;
+                //     }, 300)
+
+                // } else if(top > (sizes.y/2) && !scrollBlockY && (lastY && (lastY-window.pageYOffset)<0)){
+
+                //     console.log(`вниз`)
+
+                //     scrollBlockY = true;
+
+                //     let line =          document.querySelector(`.line`)
+                //     let newline =       line.cloneNode(true);
+                //     line.parentElement.append(newline);
+
+                //     setTimeout(()=>{
+                //         scrollBlockY =  false;
+                //     },300)
+
+                // }
+
+                if (lastX) {
+                    if (lastX > 0) {
+                        if (left < -1 * (sizes.x / 2) && !scrollBlockX) {
+
+                            console.log(`влево`)
+
+                            scrollBlockX = true;
+
+                            let line = document.querySelector(`.tile`)
+
+                            let newline = [line.cloneNode(true), line.cloneNode(true), line.cloneNode(true)];
+
+                            document.querySelectorAll(`.fl`).forEach((c, i) => c.prepend(newline[i]));
+
+                            setTimeout(() => {
+                                scrollBlockX = false;
+                            }, 300)
+                        }
+                    } else if (lastX < 0) {
+                        if (left > 0 && !scrollBlockX) {
+
+                            console.log(`вправо`)
+
+                            scrollBlockX = true;
+
+                            let line = document.querySelector(`.tile`)
+
+                            let newline = [line.cloneNode(true), line.cloneNode(true), line.cloneNode(true)];
+
+                            document.querySelectorAll(`.fl`).forEach((c, i) => c.append(newline[i]));
+
+                            setTimeout(() => {
+                                scrollBlockX = false;
+                            }, 300)
+                        }
+                    }
+                }
+
+                lastY = window.pageYOffset;
+                lastX = window.pageXOffset;
+
+                // document.querySelector(`#v`).innerHTML = `${window.pageXOffset}<br>${window.pageYOffset}<br>${sizes.y}`
+            }
+            setInterval(() => {
+                if (scrolling) {
+                    if (cpy == window.pageYOffset) {
+                        scrollEnd();
+                        scrolling = false;
+                    } else {
+                        cpy = window.pageYOffset
+                    }
+                    if (cpx == window.pageXOffset) {
+                        scrollEnd();
+                        scrolling = false;
+                    } else {
+                        cpx = window.pageXOffset
+                    }
+                }
+            }, 300)
+        }, 100)
+    }, 1000)
+
+
+
 }
 
-function scrollEnd(){
+function scrollEnd() {
 
-    
+
     scrollBlockY = false;
     scrollBlockX = false;
-    
-    let left = window.pageXOffset- sizes.x*2;
-    let top = window.pageYOffset - sizes.y*2;
+
+    let left = window.pageXOffset - sizes.x * 2;
+    let top = window.pageYOffset - sizes.y * 2;
 
     window.scrollTo({
-        left:window.pageXOffset%sizes.x+sizes.x,
-        top:window.pageYOffset%sizes.y+sizes.y
+        left: window.pageXOffset % sizes.x + sizes.x,
+        top: window.pageYOffset % sizes.y + sizes.y
     })
-        
+
     try {
-        document.querySelectorAll(`.fl`).forEach((e,i)=>{
-            e.querySelectorAll(`.tile`).forEach((t,i)=>{
-                if(i>2) t.remove()
+        document.querySelectorAll(`.fl`).forEach((e, i) => {
+            e.querySelectorAll(`.tile`).forEach((t, i) => {
+                if (i > 2) t.remove()
             })
-        })    
+        })
     } catch (error) {
         // alert(error)
     }
 
     try {
-        document.querySelectorAll(`.fl`).forEach((e,i)=>{
-            if(i>2) e.remove();
-        })    
+        document.querySelectorAll(`.fl`).forEach((e, i) => {
+            if (i > 2) e.remove();
+        })
     } catch (error) {
         // alert(error)
     }
 }
-
